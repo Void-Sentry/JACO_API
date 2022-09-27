0a6004f84287836f2ad92a0c25de779eb41cde29
teste <teste@teste.com>
2022-09-27 14:55:35 -0300

    ft: infra

--- src/main/java/com/jaco/api/infrastructure/ApiApplication.java
+++ src/main/java/com/jaco/api/infrastructure/ApiApplication.java
@@ -1,9 +1,11 @@
-package com.jaco.api;
+package com.jaco.api.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ApiApplication {
 
 	public static void main(String[] args) {
